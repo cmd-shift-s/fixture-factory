36c0db30e9914a5c0f560423bfbf73e01437fc85
gongzza <gz.us.to@gmail.com>
2018-02-07 10:29:21 +0900

    :sparkles: Dependency bulma and font-awesome
    Refs:
    - https://bulma.io
    - https://fontawesome.com

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
-  <div id="app">
+  <section class="section has-text-centered">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <h1 class="subtitle">{{ msg }}</h1>
+    <h2 >Essential Links</h2>
+    <span class="tag is-white"><a href="https://vuejs.org" target="_blank">Core Docs</a></span>
+    <span class="tag is-white"><a href="https://forum.vuejs.org" target="_blank">Forum</a></span>
+    <span class="tag is-white"><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></span>
+    <span class="tag is-white"><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></span>
+    <h2 >Ecosystem</h2>
+    <span class="tag is-white"><a href="http://router.vuejs.org/" target="_blank">vue-router</a></span>
+    <span class="tag is-white"><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></span>
+    <span class="tag is-white"><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></span>
+    <span class="tag is-white"><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></span>
+  </section>
 </template>
 
 <script>
@@ -29,32 +25,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
