bd38589548341634c6b78203df01f772e23ebd8c
gongzza <gz.us.to@gmail.com>
2018-02-07 10:48:43 +0900

    :wrench: Config webpack
    append chunkhash to output.filename

--- webpack.config.js
+++ webpack.config.js
@@ -63,6 +63,7 @@ module.exports = {
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
+  module.exports.output.filename = 'js/build.js?[chunkhash]'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
