aec2742c230d025a811ebee6387023cdc09a292e
gongzza <gz.us.to@gmail.com>
2018-02-07 17:19:01 +0900

    :sparkles: Add date format
    date format 형식을 지정할 수 있는 기능 추가
    - dependency momentjs
    - {{fake}}:date(format) 형식으로 사용
    
    Refs:
    - http://momentjs.com

--- src/faker.js
+++ src/faker.js
@@ -1,4 +1,5 @@
 import { fake } from 'faker'
+import moment from 'moment'
 import * as utils from './utils'
 
 export function makeMocks(query, cnt) {
@@ -21,16 +22,24 @@ export function range(a, b) {
 }
 
 export function fakeQuery(query, cnt) {
-  if (/{{\w+\.\w+}}:(\d)/.test(query)) {
-    const regexp = new RegExp(/({{\w+\.\w+}}):(\d)/g)
+  if (/{{\w+\.\w+}}:/.test(query)) {
+    const regexp = new RegExp(/({{\w+\.\w+}}):(\d+|date\([\w\s:-]+\))/g)
     let m, q = query;
     const mocks = []
     while ((m = regexp.exec(query))) {
       q = q.replace(m[0], '%s')
       mocks.push((() => {
-        const len = parseInt(m[2], 10)
-        const items = makeMocks(m[1], len)
-        return () => items[utils.getRandomArbitrary(0, len)]
+        if (/date\([\w\s:-]+\)/.test(m[2])) {
+          const query = m[1]
+          const format = /date\((.*)\)/.exec(m[2])[1]
+          return () => moment(fake(query)).format(format)
+        } else if (/\d+/){
+          const len = parseInt(m[2], 10)
+          const items = makeMocks(m[1], len)
+          return () => items[utils.getRandomArbitrary(0, len)]
+        }
+
+        return () => m[0]
       })())
     }
     const results = []
